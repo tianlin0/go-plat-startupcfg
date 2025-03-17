390a216d28418f199e81cad31f1546ef107be2bb
tianlin0 <tianlin0@qq.com>
2025-03-17 15:13:35 +0800

    fix: 新增翻译语言设置Ctx

--- startupcfg/language_default.go
+++ startupcfg/language_default.go
@@ -3,11 +3,13 @@ package startupcfg
 import (
 	"github.com/nicksnyder/go-i18n/v2/i18n/template"
 	"github.com/tianlin0/go-plat-utils/templates"
+	"golang.org/x/net/context"
 	"sync"
 )
 
 var (
 	langLock          sync.Mutex
+	i18nCtxKeyName    = "ctx_i18n_key_name"
 	defaultTranslator *i18nTranslator
 )
 
@@ -62,4 +64,22 @@ func I18nTranslate(tag string, key string, templateData ...any) string {
 		return defaultTranslator.TranslateByTag(tag, key, nil)
 	}
 	return defaultTranslator.TranslateByTag(tag, key, templateData[0])
+}
+
+// I18nCtxWithTag 设置当前的语言
+func I18nCtxWithTag(ctx context.Context, tag string) context.Context {
+	return context.WithValue(ctx, i18nCtxKeyName, tag)
+}
+
+// I18nTranslateCtx Ctx翻译
+func I18nTranslateCtx(ctx context.Context, key string, templateData ...any) string {
+	tag := ctx.Value(i18nCtxKeyName)
+	tagStr := ""
+	if tagTemp, ok := tag.(string); ok {
+		tagStr = tagTemp
+	}
+	if templateData == nil || len(templateData) == 0 {
+		return defaultTranslator.TranslateByTag(tagStr, key, nil)
+	}
+	return defaultTranslator.TranslateByTag(tagStr, key, templateData[0])
 }
