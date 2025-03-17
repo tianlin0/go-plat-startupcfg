f4928968ce438f12e55737d099019448f9f68f4f
tianlin0 <tianlin0@qq.com>
2025-03-17 14:06:40 +0800

    fix: 新增翻译语言设置

--- startupcfg/language_default.go
+++ startupcfg/language_default.go
@@ -31,13 +31,13 @@ func (d *defaultParser) Execute(data any) (string, error) {
 }
 
 // NewI18nFile 初始化
-func NewI18nFile(yamlFile string, tag string) (*i18nTranslator, error) {
+func NewI18nFile(yamlFile string, defaultTagString string) (*i18nTranslator, error) {
 	langLock.Lock()
 	defer langLock.Unlock()
 	if defaultTranslator == nil {
 		defaultTranslator = new(i18nTranslator)
 	}
-	err := defaultTranslator.InitFile(yamlFile, tag)
+	err := defaultTranslator.InitFile(yamlFile, defaultTagString)
 	if err != nil {
 		return defaultTranslator, err
 	}
@@ -47,7 +47,7 @@ func NewI18nFile(yamlFile string, tag string) (*i18nTranslator, error) {
 }
 
 // DefaultI18nTranslator 默认翻译器
-func DefaultI18nTranslator(key string, templateData any) *i18nTranslator {
+func DefaultI18nTranslator() *i18nTranslator {
 	langLock.Lock()
 	defer langLock.Unlock()
 	if defaultTranslator == nil {
@@ -57,9 +57,9 @@ func DefaultI18nTranslator(key string, templateData any) *i18nTranslator {
 }
 
 // I18nTranslate 翻译
-func I18nTranslate(key string, templateData ...any) string {
+func I18nTranslate(tag string, key string, templateData ...any) string {
 	if templateData == nil || len(templateData) == 0 {
-		return defaultTranslator.Translate(key, nil)
+		return defaultTranslator.TranslateByTag(tag, key, nil)
 	}
-	return defaultTranslator.Translate(key, templateData[0])
+	return defaultTranslator.TranslateByTag(tag, key, templateData[0])
 }
--- startupcfg/language_test.go
+++ startupcfg/language_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/tianlin0/go-plat-startupcfg/startupcfg"
 	"testing"
+	"time"
 )
 
 type AAA struct {
@@ -12,13 +13,34 @@ type AAA struct {
 
 func TestAesCbc(t *testing.T) {
 
+	// 初始化
 	_, err := startupcfg.NewI18nFile("language.yaml", "zh")
 	if err != nil {
 		return
 	}
-	mm := startupcfg.I18nTranslate("aaa.bbbb.ccccc", &AAA{
+
+	mm := startupcfg.I18nTranslate("zh", "aaa.bbbb.ccccc", &AAA{
+		Name: "mmmm",
+	})
+	fmt.Println(mm)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			mm := startupcfg.I18nTranslate("en", "aaa.bbbb.ccccc", &AAA{
+				Name: "mmmm",
+			})
+			fmt.Println(mm)
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	time.Sleep(5 * time.Second)
+
+	mm = startupcfg.I18nTranslate("zh", "aaa.bbbb.ccccc", &AAA{
 		Name: "mmmm",
 	})
 
 	fmt.Println(mm)
+
+	time.Sleep(time.Minute)
 }
